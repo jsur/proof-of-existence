e03815a5fdad479214096dd2bfdef713229b238e
julius.suominen <julius.suominen@gmail.com>
2021-09-21 14:52:08 +0300

    add poe3 with mapping

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,11 @@
 const ProofOfExistence1 = artifacts.require('./ProofOfExistence1.sol');
 const ProofOfExistence2 = artifacts.require('./ProofOfExistence2.sol');
+const ProofOfExistence3 = artifacts.require('./ProofOfExistence3.sol');
 
 module.exports = function(deployer) {
-    [ProofOfExistence1, ProofOfExistence2].forEach(item => deployer.deploy(item));
+    [
+        ProofOfExistence1,
+        ProofOfExistence2,
+        ProofOfExistence3
+    ].forEach(item => deployer.deploy(item));
 };
