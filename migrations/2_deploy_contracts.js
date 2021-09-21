37ea426bb60c9891c7509b6336b75b2f33367603
julius.suominen <julius.suominen@gmail.com>
2021-09-21 14:42:49 +0300

    add poe2 without mappings

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
 const ProofOfExistence1 = artifacts.require('./ProofOfExistence1.sol');
+const ProofOfExistence2 = artifacts.require('./ProofOfExistence2.sol');
 
 module.exports = function(deployer) {
-    deployer.deploy(ProofOfExistence1);
+    [ProofOfExistence1, ProofOfExistence2].forEach(item => deployer.deploy(item));
 };
